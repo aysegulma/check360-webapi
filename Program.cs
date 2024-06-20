ad3d6d020f1abcd2308d2a6916dced5adde928ed
aysegulma <aysegul.mandaci@gmail.com>
2024-06-20 12:47:29 +0200

    Comment changed

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-//aysegulma
+//aysegulma - 2024
 app.UseCors(x => x
                     .AllowAnyMethod()
                     .AllowAnyHeader()
